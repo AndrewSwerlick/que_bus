45ebc2addd0f3cfbbc497991a4aa0de587f42f1d
Andrew Swerlick <andrew.swerlick@hbdi.com>
2014-09-05 08:40:24 -0400

    setting up block fo subscription test

--- lib/que_bus/bus.rb
+++ lib/que_bus/bus.rb
@@ -1,11 +1,23 @@
+require 'securerandom'
+
 module QueBus
   class Bus
-    def publish(channel, message)
+    def initialize
+      @subscribers = []
+    end
+
+    def publish(channel=nil, message)
 
     end
 
-    def subscribe(channel)
+    def subscribe(id=nil, channel=nil)
+      sub_id = id || SecureRandom.uuid
+      @subscribers << sub_id
+      sub_id
+    end
 
+    def subscribers
+      @subscribers.clone
     end
   end
 end
--- spec/lib/que_bus/bus_spec.rb
+++ spec/lib/que_bus/bus_spec.rb
@@ -4,12 +4,45 @@ describe Bus do
   describe "when we create a new bus" do
     let(:bus) {Bus.new}
 
-    it "allows us to subscribe to a channel" do
-      bus.subscribe("foo")
+    describe "and subscribe the bus" do
+      before do
+        @id = bus.subscribe
+      end
+
+      it "gives us a subscription id" do
+        @id.wont_be_nil
+      end
+
+      it "shows us as one of the subscribers" do
+        bus.subscribers.must_include @id
+      end
+    end
+
+    describe "when we subscribe to the bus with our own id" do
+      before do
+        @id = bus.subscribe("test")
+      end
+
+      it "gives us the same id back" do
+        @id.must_equal "test"
+      end
+
+      it "shows us as one of the subscribes using that id" do
+        bus.subscribers.must_include @id
+      end
+    end
+
+
+    it "allows us to subscribe to all channels on the bus" do
+      bus.subscribe
     end
 
     it "allows us to publish to a channel" do
       bus.publish("foo", {test: "test"})
+    end
+
+    it "allows us to publish to all listeners on the bus" do
+      bus.publish({test:"test"})
     end
   end
 end
