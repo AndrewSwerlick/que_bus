d427c27aaaf6ccf9ce38b61ac5ff57f63dd33b57
Andrew Swerlick <andrew.swerlick@hbdi.com>
2014-09-15 23:14:20 -0400

    putting generator in the correct module

--- lib/generators/que_bus/install_generator.rb
+++ lib/generators/que_bus/install_generator.rb
@@ -1,18 +1,20 @@
-class InstallGenerator < Rails::Generators::Base
-  def create_initializer_file
-    create_file "config/initializers/que_bus.rb", self.contents
-  end
+module QueBus
+  class InstallGenerator < Rails::Generators::Base
+    def create_initializer_file
+      create_file "config/initializers/que_bus.rb", self.contents
+    end
 
-  def self.contents
-    %{
-      #Turns the bus off. This is default for rails because generally the bus will run in a rake task
-      QueBus.mode = :off
+    def self.contents
+      %{
+        #Turns the bus off. This is default for rails because generally the bus will run in a rake task
+        QueBus.mode = :off
 
-      #write your subscribe logic here. This file will be loaded during initialization of the rake task.
-      #example
-      #QueBus.subscribe do
-      # ..respond to published events here
-      #end
-    }
+        #write your subscribe logic here. This file will be loaded during initialization of the rake task.
+        #example
+        #QueBus.subscribe do
+        # ..respond to published events here
+        #end
+      }
+    end
   end
 end
